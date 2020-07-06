c77cdf4ae5cd01c85e4bf16bff9efe87360f2ede
Dries Bos <info@driesbos.com>
2020-07-06 20:53:38 +0200

    fix: SSR generate

---
+++ pages/over/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class>
+    <component
+      :is="story.content.component | dashify"
+      v-if="story.content.component"
+      :key="story.content._uid"
+      :blok="story.content"
+    />
+    <blok-footer-form />
+  </div>
+</template>
+
+<script>
+import storyblokLivePreview from "@/mixins/storyblokLivePreview"
+
+export default {
+  mixins: [storyblokLivePreview],
+  asyncData(context) {
+    let version =
+      context.query._storyblok || context.isDev ? "draft" : "published"
+    return context.app.$storyapi
+      .get("cdn/stories/over", {
+        version: version
+      })
+      .then(res => {
+        return res.data
+      })
+      .catch(res => {
+        if (!res.response) {
+          console.error(res)
+          context.error({
+            statusCode: 404,
+            message: "Failed to receive content form api"
+          })
+        } else {
+          console.error(res.response.data)
+          context.error({
+            statusCode: res.response.status,
+            message: res.response.data
+          })
+        }
+      })
+  },
+  data() {
+    return {
+      story: { content: {} }
+    }
+  }
+}
+</script>
