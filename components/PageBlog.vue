e416348445e11d68b0156885cdec3a7bd32eb243
Dries Bos <info@driesbos.com>
2020-07-22 09:38:35 +0200

    fix: blog components spacing

--- components/PageBlog.vue
+++ components/PageBlog.vue
@@ -6,16 +6,6 @@
       :key="blok._uid"
       :blok="blok"
     ></component>
-    <div class="articles-List blok">
-      <!-- <h1 v-if="blok.title" class="blok-Title">{{ blok.title }}</h1>
-      <markdown-item v-if="blok.text" class="blok-Text" :input="blok.text" />-->
-      <!-- <component
-        :is="blok.component | dashify"
-        v-for="blok in blok.body"
-        :key="blok._uid"
-        :blok="blok"
-      ></component>-->
-    </div>
   </div>
 </template>
 
