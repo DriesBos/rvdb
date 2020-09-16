8578cd7d1f49b973eac8ebea79a427a5551a4aa9
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-16 13:24:31 +0200

    refactor: component naming

--- plugins/components.js
+++ plugins/components.js
@@ -3,7 +3,7 @@ import AnchorNav from "~/components/AnchorNav.vue"
 import ExpandableItem from "~/components/ExpandableItem.vue"
 import ExpandableList from "~/components/ExpandableList.vue"
 import Hyperlink from "~/components/Hyperlink.vue"
-import Madeby from "~/components/Madeby.vue"
+import Colofon from "~/components/Colofon.vue"
 import Page from "~/components/Page.vue"
 import PageBlog from "~/components/PageBlog.vue"
 import Text from "~/components/Text.vue"
@@ -12,7 +12,7 @@ Vue.component("blok-anchor-nav", AnchorNav)
 Vue.component("blok-expandable-item", ExpandableItem)
 Vue.component("blok-expandable-list", ExpandableList)
 Vue.component("blok-hyperlink", Hyperlink)
-Vue.component("blok-madeby", Madeby)
+Vue.component("blok-colofon", Colofon)
 Vue.component("blok-page", Page)
 Vue.component("blok-page-blog", PageBlog)
 Vue.component("blok-text", Text)
